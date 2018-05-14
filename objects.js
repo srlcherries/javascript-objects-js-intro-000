7fecf1b3a6363ab0e2a65cd695d960bb47df55ed
Sherry Lin <sherrylin222@gmail.com>
2018-05-14 00:04:56 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
-var playlist = newObject();
+var playlist = newObject({nsync: "it's gonna be me"});
 /*function updatePlaylist(playlist, 'artist name', 'song title') {
   return Object.assign({}, playlist, { ['artist name']: 'song title' });
 }*/
